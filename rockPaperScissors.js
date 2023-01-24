00f3ec010a109332bc5491449a33b944a2ec6b89
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-01-24 18:06:33 +0200

    Update rockPaperScissors.js
    finish

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,37 +1,42 @@
-function game() {
+function game(playerTurn) {
 
-    let playerTurn = 0;
     let computerTurn = 0;
 
     const rock = "Rock";
     const paper = "Paper";
-    const scissors = "scissors";
+    const scissors = "Scissors";
 
     if (playerTurn == "r" || playerTurn == "rock") {
         playerTurn = rock;
+        console.log(`You chooses ${rock}`);
     } else if (playerTurn == "p" || playerTurn == "paper") {
         playerTurn = paper;
+        console.log(`You chooses ${paper}`)
     } else if (playerTurn == "s" || playerTurn == "scissorsissior") {
         playerTurn = scissors;
+        console.log(`You chooses ${scissors}`)
     } else {
         console.log("Invali Input. Try Again...");
     }
     let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
 
     switch (computerRandomNumber) {
-        case computerTurn: "Rock";
-            computerTurn = rock; break;
-        case computerTurn: "Paper";
-            computerTurn = paper; break;
-        case computerTurn: "scissors"
-            computerTurn = scissors; break;
+        case 1: computerTurn = rock; break;
+        case 2: computerTurn = paper; break;
+        case 3: computerTurn = scissors; break;
     }
     console.log(`The computer chose ${computerTurn}.`);
 
     if ((playerTurn === rock && computerTurn === scissors)
-    || (playerTurn === paper && computerTurn === rock) 
-    || (playerTurn === scissors && computerTurn === paper)){
+        || (playerTurn === paper && computerTurn === rock)
+        || (playerTurn === scissors && computerTurn === paper)) {
         console.log("You win!");
-    }
+    } else if ((computerTurn === rock && playerTurn === scissors)
+        || (computerTurn === paper && playerTurn === rock)
+        || (computerTurn === scissors && playerTurn === paper)){
+            console.log("You lose!");
+        }else{
+            console.log("This game was a draw!");
+        }
 }
-game();
+game("p");
